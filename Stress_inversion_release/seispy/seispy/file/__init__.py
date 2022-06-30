82e2c82ae93e719221190a55fdc91355fd664c43
yunndlalala <845040686@qq.com>
2022-06-30 17:25:39 +0800

    Delete Stress_inversion_release directory

--- Stress_inversion_release/seispy/seispy/file/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-@version:
-author:yunnaidan
-@time: 2019/07/21
-@file: __init__.py.py
-@function:
-"""
--- Stress_inversion_release/seispy/seispy/time_utils.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-@author: yunnaidan
-@time: 2021/06/06
-@file: time_utils.py
-"""
